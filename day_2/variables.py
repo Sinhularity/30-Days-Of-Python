b668ce5594ddbd748ef68f5d743997e3140fd747
Sinhularity <114500120+Sinhularity@users.noreply.github.com>
2023-03-20 17:48:35 -0600

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,14 @@
+
+#Day 2: 30 Days of python programming
+
+first_name = 'Kevin'
+last_name = 'García'
+full_name = 'Kevin S. Frias García'
+country = 'Mexico'
+city = 'Veracruz'
+age = 19
+year = 2003
+is_married = False
+is_true = True
+is_light_on = True
+variable1, tiempo = 3, '3.pm'
